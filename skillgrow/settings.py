96d783a2a167b4f6ba945b409013e9e226d77251
giorgi <gsturua892@gmail.com>
2025-06-07 02:26:40 +0400

    settingschange

--- skillgrow/settings.py
+++ skillgrow/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$jnqkoq^qk$_+_e3x8wtwp%6c5$4ly%z1=w#_wdz7#%wp_ico7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['skillgrow.onrender.com']
 
 
 # Application definition
